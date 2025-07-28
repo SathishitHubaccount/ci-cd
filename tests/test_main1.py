9fb22874a1e3ddb6e1e3496ad5218ce5c040550d
SathishitHubaccount <sathish2352004@gmail.com>
2025-07-28 11:37:27 +0530

    Created-final

--- tests/test_main1.py
+++ tests/test_main1.py
@@ -4,7 +4,7 @@ from src.main import anagram,length,nothing
 def test_anagram():
     assert anagram("ab","ba")==True
     assert anagram("abc","bca")==True
-    assert anagram("asb","bsssa")==True
+    assert anagram("asb","bsssa")==False
     assert anagram("jhdihdihiod","jiidsdoao")==False
     assert anagram("Hi","")==False
 
